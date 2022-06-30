a3c8b8a93abc41435e375db77f91edc332d234fb
Kevin Lu <85902071+ssasukek@users.noreply.github.com>
2022-06-30 14:21:46 -0700

    new

--- parkourPanda/Assets/Scripts/Count.cs
+++ parkourPanda/Assets/Scripts/Count.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Count : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+   public float count = 0;
+    void OnTriggerEnter(Collider other){
+        count++;
+        Debug.Log(count);
     }
 }
