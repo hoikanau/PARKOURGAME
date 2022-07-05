b68603fb0f9957c43dc7bdf7b21596dcabe03171
jerry <hoikanau123@gmail.com>
2022-07-05 10:59:25 -0700

    teleporter

--- parkourPanda/Assets/Scripts/playerTeleport.cs
+++ parkourPanda/Assets/Scripts/playerTeleport.cs
@@ -4,17 +4,7 @@ using UnityEngine;
 
 public class playerTeleport : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "sender")
